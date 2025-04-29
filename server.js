bacde0c380ec1b14dd24a201667636eaecacbf99
Fausto Izquierdo <fizquiv@gmail.com>
2025-04-29 09:18:34 -0600

    add public/html and database content, code first endpoint

---
+++ server.js
@@ -0,0 +1,22 @@
+"use strict";
+
+import express from "express";
+import { fs } from "fs";
+
+const port = 3000;
+const app = express();
+
+app.use(express.json());
+app.use(express.static("./public"));
+
+app.get("/", (req, res) => {
+  fs.readFile("./public/html/index.html", (err, html) => {
+    if (err) {
+      res.status(500).send("Error reading file");
+      return;
+    }
+    console.log("Sending page");
+    res.send(html);
+    console.log("Page sent!");
+  });
+});
