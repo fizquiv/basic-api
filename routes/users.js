52aaabf30d764645584f2ba8e9514a99d1bc4f9b
Fausto Izquierdo <fizquiv@gmail.com>
2025-04-30 20:24:27 -0600

    program get user by id endpoint

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,15 @@ import { readDataBase, writeDataBase } from "../data/fileHandler.js";
 
 export async function getAllusers() {}
 
-export async function getUserById(id) {}
+export async function getUserById(id) {
+  const db = await readDataBase();
+  const user = db.users.find((user) => user.id === id);
+  if (!user) return null;
+  return {
+    ...user,
+    items: user.items.map((id) => db.items.find((item) => item.id === id)),
+  };
+}
 
 export async function addUser(newUser) {}
 
