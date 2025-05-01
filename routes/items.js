428237a418429cd779c69af979feedc1a761e3d7
Fausto Izquierdo <fizquiv@gmail.com>
2025-04-30 18:25:16 -0600

    program adding item endpoint

--- routes/items.js
+++ routes/items.js
@@ -4,7 +4,19 @@ export async function getAllItems() {}
 
 export async function getItemById(id) {}
 
-export async function addItem() {}
+export async function addItem(newItem) {
+  const db = await readDataBase();
+
+  const requiredFields = ["id", "name", "type", "effect"];
+  const missingFields = requiredFields.filter((field) => !newItem[field]);
+  if (missingFields.length > 0) return { error: "Missing fields" };
+  if (db.items.find((item) => item.id === newItem.id)) {
+    return { error: "item already exists" };
+  }
+  db.items.push(newItem);
+  await writeDataBase(db);
+  return { message: "Item added successfully!" };
+}
 
 export async function updateItem(id) {}
 
--- server.js
+++ server.js
@@ -36,6 +36,12 @@ app.get("/", (req, res) => {
   });
 });
 
+app.post("/api/items", async (req, res) => {
+  const result = await addItem(req.body);
+  if (result.error) return res.status(409).json(result);
+  res.status(201).json(result);
+});
+
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
