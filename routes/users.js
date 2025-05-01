b2eeccfbf066d323e1a9e9703023d57714757cb7
Fausto Izquierdo <fizquiv@gmail.com>
2025-04-30 20:51:56 -0600

    program delete user endpoint

--- routes/users.js
+++ routes/users.js
@@ -8,4 +8,12 @@ export async function addUser(newUser) {}
 
 export async function updateUser(id, update) {}
 
-export async function deleteUser(id) {}
+export async function deleteUser(id) {
+  const db = await readDataBase();
+  const index = db.users.findIndex((user) => user.id === id);
+  if (index === -1) return { error: "User not found." };
+
+  db.users.splice(index, 1);
+  await writeDataBase(db);
+  return { message: "User deleted successfully!" };
+}
